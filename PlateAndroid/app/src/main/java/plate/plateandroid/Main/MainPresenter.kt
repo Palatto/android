961f701784e12d8b436e963ac68ed0688dd9dff0
rennerll00 <renner@stanford.edu>
2017-10-29 19:41:15 -0700

    Basic starter for Android

---
+++ PlateAndroid/app/src/main/java/plate/plateandroid/Main/MainPresenter.kt
@@ -0,0 +1,18 @@
+package plate.plateandroid.Main
+
+/**
+ * Created by rennerll on 10/20/17.
+ */
+class MainPresenter: MainContract.Presenter {
+
+    lateinit var view: MainContract.View
+
+    override fun init(view: MainContract.View) {
+        this.view = view
+    }
+
+    override fun showMessage() {
+        view.showToast("Let's go!!!")
+    }
+
+}
